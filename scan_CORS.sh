0a7b52922c50578b203e423134a66a5646c8425f
Ismael Oliveira <70406460+ismael0liveira@users.noreply.github.com>
2023-05-30 12:18:03 +0000

    Update scan_CORS.sh

--- scan_CORS.sh
+++ scan_CORS.sh
@@ -20,7 +20,7 @@ data=$(date +"%H:%M:%S");
 echo -e "\033[1;34;40m"$data"\033[m \033[05;33m<SCAN CORS - MISCONFIGURATION />>\033[m\n";
 
 
-cat $pasta"/scanner/"$1"/resultados/subsResolvidos.txt" | python3 /home/ninja/apps/Corsy/corsy.py > $pasta"/scanner/"$1"/resultados/resultados_CORS.txt";
+cat $pasta"/scanner/"$1"/resultados/subsResolvidos.txt" | python3 tool_CORSY/corsy.py > $pasta"/scanner/"$1"/resultados/resultados_CORS.txt";
 
 
 cat $pasta"/scanner/"$1"/resultados/subsResolvidos.txt" | CorsMe -t 70 >> $pasta"/scanner/"$1"/resultados/resultados_CORS.txt";
