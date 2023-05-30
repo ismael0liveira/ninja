f6b077501b26d06392dc965cadf30b1d9014cede
Ismael Oliveira <70406460+ismael0liveira@users.noreply.github.com>
2023-05-30 12:57:04 +0000

    Update scan_LOG4SHELL.sh

--- scan_LOG4SHELL.sh
+++ scan_LOG4SHELL.sh
@@ -5,4 +5,14 @@ data=$(date +"%H:%M:%S");
 echo -e "\033[1;34;40m"$data"\033[m \033[05;33m<<LOG 4SHELL - /LogMePwn/>\033[00;37m";
 pasta=$(pwd);
 
-./tool_LOG_4_SHELL/lmp -email 01ismaeloliveira@gmail.com -file $pasta"/scanner/"$1"/resultados/subs.txt" -threads 100
+echo -e "\n\033[33;1mPara prosseguir, digite um e-mail\033[m";
+
+dominio=$(zenity --forms    \
+        --title="Formulário"    \
+        --text="Formulário Hacking" \
+        --add-entry="Meu email" \
+        --separator="," \
+        --ok-label="Hackear"
+);
+
+./tool_LOG_4_SHELL/lmp -email $email -file $pasta"/scanner/"$1"/resultados/subs.txt" -threads 100
