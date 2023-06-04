1f08c458d8a0fa0fb05776998f069015d3d7e167
Ismael Oliveira <70406460+ismael0liveira@users.noreply.github.com>
2023-06-04 09:52:06 -0300

    Delete scan_SMUGGLER.sh

--- scan_SMUGGLER.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-data=$(date +"%H:%M:%S");
-echo -e "\033[1;34;40m"$data"\033[m \033[05;33m<<SCANEANDO HTTP SMUGGLING - SMUGGLER/>\033[00;37m";
-
-caminho=$(pwd);
-
-for url in $(cat $caminho"/scanner/"$1"/resultados/subsResolvidos.txt");do python3 tool_SMUGGLER/smuggler.py -u $url -l $caminho"/scanner/"$1"/resultados/resultados_smuggling.txt";done
