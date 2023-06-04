cfcf74706190fbab8f017d5265a3ad5be58147c2
Ismael Oliveira <70406460+ismael0liveira@users.noreply.github.com>
2023-06-04 09:49:41 -0300

    Delete scan_VULMAP.sh

--- scan_VULMAP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pasta=$(pwd);
-
-python3 tool_VULMAP/vulmap.py -f $pasta"/scanner/"$1"/resultados/subs.txt" -t 50 --debug --output-text $pasta"/scanner/"$1"/resultados/resultados_VULMAP.txt"
