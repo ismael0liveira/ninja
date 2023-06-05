c21d1e2887863ee1e8a83a0729e9c5b98d7b2ac1
Ruan <xslackx@live.fr>
2023-06-05 08:57:37 -0300

    Update banner.py

--- banners/banner.py
+++ banners/banner.py
@@ -1,9 +1,19 @@
-import random
-import os
+import os, re, random
+motdBanners = "motd/"
 
-numero=random.randint(1,7)
-numeroBanner=random.randint(1,9)
+def insertMotd(motd):
+    motds = []
+    if re.match(r"ninja[0-9].txt", motd):
+        motds.append(motd)
+    if motds != None:
+        return motds
 
-banner="banners/ninja"+str(numero)+".txt"
+if os.access(motdBanners, os.R_OK):
+    allFiles = os.listdir(motdBanners)
+    absPath = os.getcwd()
+    motds = list(map(insertMotd, allFiles))
+    rnd = random.randint(1,len(motds)-1)
+    file = str(absPath+f"/{motdBanners}"+motds[rnd][0])
 
-os.system("cat "+banner)
+    with open(file) as banner:
+        print(banner.read())
