7926c2ad0b65926402b3d9c80d95e3e8052cc4ad
Ismael Oliveira <70406460+ismael0liveira@users.noreply.github.com>
2023-06-24 18:26:31 +0000

    Add files via upload

---
+++ get_urls.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+pasta=$(pwd);
+
+data=$(date +"%H:%M:%S");
+echo -e "\033[1;34;40m"$data"\033[m \033[05;33m<THREADS - GET URLS />>\033[m\n";
+
+
+cat resolvidosDois.txt > $pasta"/scanner/"$1"/resultados/"subsResolvidos.txt;
+
+
+for n in $(seq 1 100);do
+	for url in $(cat $pasta"/scanner/"$1"/resultados/subsResolvidos.txt" | sed 's/http.*\/\///');do
+		curl "https://otx.alienvault.com/api/v1/indicator/hostname/"$url"/url_list?limit=500&page="$n -s | jq -r ".url_list" | jq .[].url | sed 's/"//' >> $pasta"/scanner/"$1"/resultados/"urls_s.txt &
+
+	echo -e "\n\n\033[32;1mTESTANDO: \033[m https://otx.alienvault.com/api/v1/indicator/hostname/"$url"/url_list?limit=500&page="$n"\n";
+	done;
+done;
+
+for url in $(cat $pasta"/scanner/"$1"/resultados/subsResolvidos.txt" | sed 's/http.*\/\///');do
+	echo -e "\n\n\033[32;1mTESTANDO: \033[m"$url"\n\n";
+	curl "http://web.archive.org/cdx/search/cdx?url="$url"*&output=txt" -s | awk -F " " '{print $3}' | sort -u >> $pasta"/scanner/"$1"/resultados/"urls_s.txt &
+done
+
+bash;
