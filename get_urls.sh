a8a5164d172e0b74edabb1e6155ebdd708ae1534
Ismael Oliveira <70406460+ismael0liveira@users.noreply.github.com>
2023-06-24 15:26:47 -0300

    Update get_urls.sh

--- get_urls.sh
+++ get_urls.sh
@@ -6,9 +6,6 @@ data=$(date +"%H:%M:%S");
 echo -e "\033[1;34;40m"$data"\033[m \033[05;33m<THREADS - GET URLS />>\033[m\n";
 
 
-cat resolvidosDois.txt > $pasta"/scanner/"$1"/resultados/"subsResolvidos.txt;
-
-
 for n in $(seq 1 100);do
 	for url in $(cat $pasta"/scanner/"$1"/resultados/subsResolvidos.txt" | sed 's/http.*\/\///');do
 		curl "https://otx.alienvault.com/api/v1/indicator/hostname/"$url"/url_list?limit=500&page="$n -s | jq -r ".url_list" | jq .[].url | sed 's/"//' >> $pasta"/scanner/"$1"/resultados/"urls_s.txt &
